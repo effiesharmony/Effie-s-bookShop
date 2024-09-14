507f36675675f5a1406efd7fdfaac7bcefe8cea5
Effie Armony <harmony.effie@gmail.com>
2024-09-14 20:07:13 +0300

    Initial commit

---
+++ js/book.controller.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function onInit(){
+    render()
+}
+function render(){
+    
+}
---
+++ js/services/book.service.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var gBooks =[
+    {
+        id: 'bg4J78',
+        title: 'The Holy Bible',
+        price: 5,
+        imgUrl: 'bible.jpg'
+    },
+    {
+        id: 'bg4J79',
+        title: 'Pinocchio',
+        price: 10,
+        imgUrl: 'bible.jpg'
+    },
+    {
+        id: 'bg4J70',
+        title: 'Little Red Riding Hood',
+        price: 15,
+        imgUrl: 'bible.jpg'
+    },
+]
+
+function getBooks(){
+    return gBooks
+}
