c4a899dc2f29a7de46da0caa4ff5ccb76f3ec022
Effie Armony <harmony.effie@gmail.com>
2024-09-15 21:37:15 +0300

    Implementing query params

--- js/book.controller.js
+++ js/book.controller.js
@@ -3,10 +3,10 @@
 const gQueryOptions = {
     filterBy: { txt: '', minPrice: 0 },
     sortBy: {},
-    page: { idx: 0, size: 4 },
 }
 
 function onInit() {
+    readQueryParams()
     renderBooks()
 }
 function renderBooks() {
@@ -25,6 +25,7 @@ function renderBooks() {
             </tr>
         `)
     elTbody.innerHTML = strHtmls.join('')
+    setQueryParams()
 }
 
 function onRemoveBook(bookId) {
@@ -68,7 +69,6 @@ function onSetFilterBy() {
     gQueryOptions.filterBy.txt = elTitle.value
     gQueryOptions.filterBy.minPrice = +elMinPrice.value
 
-    gQueryOptions.page.idx = 0
     renderBooks()
 }
 
@@ -79,4 +79,54 @@ function onSetSortBy() {
     gQueryOptions.sortBy.sortField = elsortField.value
     gQueryOptions.sortBy.descending = elDescending.checked ? -1 : 1
     renderBooks()
+}
+
+function readQueryParams() {
+    const queryParams = new URLSearchParams(window.location.search)
+    
+    gQueryOptions.filterBy = {
+        txt: queryParams.get('title') || '',
+        minPrice: +queryParams.get('price') || 0
+    }
+
+    if(queryParams.get('sortField')) {
+        const prop = queryParams.get('sortField')
+        const descending = queryParams.get('descending')
+
+        gQueryOptions.sortBy.sortField = prop
+        gQueryOptions.sortBy.descending = descending
+    }
+
+    renderQueryParams()
+}
+
+function renderQueryParams() {
+    
+    document.querySelector('.title').value = gQueryOptions.filterBy.txt
+    document.querySelector('.min-price').value = gQueryOptions.filterBy.minPrice
+    
+    const sortField = gQueryOptions.sortBy.sortField
+    const descending = +gQueryOptions.sortBy.descending
+
+    document.querySelector('.sort-by select').value = sortField || ''
+    document.querySelector('.sort-by input').checked = (descending === -1) ? true : false
+}
+
+function setQueryParams() {
+    const queryParams = new URLSearchParams()
+
+    queryParams.set('title', gQueryOptions.filterBy.txt)
+    queryParams.set('price', gQueryOptions.filterBy.minPrice)
+
+    if(gQueryOptions.sortBy.sortField) {
+        queryParams.set('sortField', gQueryOptions.sortBy.sortField)
+        queryParams.set('descending', gQueryOptions.sortBy.descending)
+    }
+
+    const newUrl = 
+        window.location.protocol + "//" + 
+        window.location.host + 
+        window.location.pathname + '?' + queryParams.toString()
+
+    window.history.pushState({ path: newUrl }, '', newUrl)
 }
